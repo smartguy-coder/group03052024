e38e16aa44486a97cf314c4234055452f3f2d13b
Василь Картичак <w.i.k.mailua@gmail.com>
2024-05-10 20:24:10 +0300

    temp

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # group03052024
 
 ## poerty
-- pip install poerty
+- pip install poetry
 - poetry config --local virtualenvs.in-project true
 - poetry init -n 
 - poetry install
 - .\.venv\Scripts\activate 
 - poetry add fastapi
+- poetry add flake8 
+- poetry add black
+- poetry add isort
 
 
 ## other
 - echo 'mkdfgjhfg' > ggg
+
+
+Ctrl - Alt - L
+Ctrl - D
--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello dude")
+from typing import Union
+
+
+def add_two_numbers(first_number: Union[int, float], second_number: int | float) -> int | float:
+    """
+    sums given numbers
+    2 + 2 => 4
+    """
+
+    result = first_number + second_number
+    return result
+
+
+def send_email(address: str, body: dict) -> None:
+
+    print("sending.....")
+
+
+def is_equal(first_number: int, second_number: int) -> bool:
+    result = first_number == second_number
+    return result
+
+
+print("-------", add_two_numbers(1.5, 99999999) + 55555)
