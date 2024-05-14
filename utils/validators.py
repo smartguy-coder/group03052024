690b0a6b16d9d610bec27866cf09b0cd38690391
v_kartychak <w@ukr.net>
2024-05-14 20:26:40 +0300

    fix utils

--- utils/validators.py
+++ utils/validators.py
@@ -1,11 +1,8 @@
 def is_valid_email(email_candidate: str) -> bool:
-    return False
-    print(f'{email_candidate.count("@")=}')
     if email_candidate.count("@") > 1:
         return False
 
     email_parts = email_candidate.strip("@").split("@")
-    print(f"{email_parts=}")
     if len(email_parts) == 2:
         return True
     return False
