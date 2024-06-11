2757a0fe9cff824f51ff7a727dca671746a7510d
vasylkartychak <w.i.k.mailua@gmail.com>
2024-06-11 21:32:57 +0300

    decorators advance

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
-from utils import email_sender
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory='templates')
+
+app = FastAPI()
 
 
-def main():
-    inner_html_body = email_sender.create_welcome_letter(
-        {
-            'name': 'Alex',
-            'hobbies': ['tennis', 'soccer'],
-            'has_car': True
-        }
-    )
-    print(inner_html_body)
-    email_sender.send_email(
-        ["test_hillel_api_mailing@ukr.net", "dduckker@ukr.net"],
-        mail_body=inner_html_body,
-        mail_subject="mail_subject",
-        # attachment='2024-05-21_199-25.png',
-    )
+books = [
+    {
+        "title": "Майстер і Маргарита",
+        "author": "Михайло Булгаков",
+        "year": 1967,
+        "genre": "Роман"
+    },
+    {
+        "title": "Тарас Бульба",
+        "author": "Микола Гоголь",
+        "year": 1835,
+        "genre": "Історичний роман"
+    },
+    {
+        "title": "Кобзар",
+        "author": "Тарас Шевченко",
+        "year": 1840,
+        "genre": "Збірка віршів"
+    },
+    {
+        "title": "Захар Беркут",
+        "author": "Іван Франко",
+        "year": 1883,
+        "genre": "Історичний роман"
+    },
+    {
+        "title": "Собор Паризької Богоматері",
+        "author": "Віктор Гюго",
+        "year": 1831,
+        "genre": "Роман"
+    }
+]
 
 
-if __name__ == "__main__":
-    main()
+
+@app.get('/api/')
+def index() -> dict:
+    return {'status': 'OK'}
+
+
+@app.get('/')
+def index_web(request: Request):
+
+    return templates.TemplateResponse('index.html', {'request': request, 'books': books, 'title': 'Main page'})
